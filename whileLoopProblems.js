16f0e6a920ca673fe38b6f2e03689fe222aabbba
Harsh R Jain <rjainharsh@gmail.com>
2021-10-06 05:06:07 +0530

    ADDED: logic to guess magic number

--- whileLoopProblems.js
+++ whileLoopProblems.js
@@ -6,8 +6,25 @@ while(Math.pow(2,number1) > Math.pow(2,i)){
     console.log("1. 2^",i," = ",Math.pow(2,i));
 }
 
+//2. 
+let magicNumber = Math.floor(Math.random() * 100);
+let low=0, high=100, middle;
+while(low <= high) {
+    middle = ~~((low+high)/2);
+    if(middle==magicNumber) break;
+    if(middle > magicNumber) {
+        console.log(`${middle} is grEater than ${magicNumber}`)
+        low = low; high= middle;
+    }
+    else {
+        console.log(`${middle} is LESSER than ${magicNumber}`)
+        low = middle; high= high;
+    }
+}
+if(middle!=magicNumber) console.log("MATCH FOUND");
+else console.log("MATCH NOT FOUND");
 
-//2.
+//3.
 let tailCount=0, headCount=0;
 while(tailCount<11 && headCount<11) {
     if(Math.random() > 0.5) {
@@ -21,7 +38,7 @@ while(tailCount<11 && headCount<11) {
 }
 console.log("2. Head count: ",headCount, " tail count: ",tailCount);
 
-//3.
+//4..
 let amount = 100;
 while(amount > 0 && amount < 200) {
     if(Math.random() > 0.5) {
