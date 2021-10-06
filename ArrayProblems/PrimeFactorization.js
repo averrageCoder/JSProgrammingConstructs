fca567c3daea3762a01c01e2ad8f65d91babbc83
Harsh R Jain <rjainharsh@gmail.com>
2021-10-07 03:02:00 +0530

    ADDED: progra, to find prime factors of a number

---
+++ ArrayProblems/PrimeFactorization.js
@@ -0,0 +1,15 @@
+let factors = [];
+let divisor = 2;
+n = Math.floor(Math.random() * 10000);
+console.log('5. Prime factors of ', n );
+while (n >= 2) {
+    if (n % divisor == 0) {
+    factors.push(divisor);
+    n = n / divisor;
+    } else {
+    divisor++;
+    }
+}
+for (let k = 0; k < factors.length; k++) {
+    console.log(factors[k]);
+} 
