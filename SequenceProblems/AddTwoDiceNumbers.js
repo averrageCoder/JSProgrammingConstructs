f915acbb773598917876e30aa3c4fe17d3a84039
Harsh R Jain <rjainharsh@gmail.com>
2021-10-06 20:50:27 +0530

    ADDED: program to add 2 random dice rolls

---
+++ SequenceProblems/AddTwoDiceNumbers.js
@@ -0,0 +1,3 @@
+let firstDiceNumber = Math.floor(Math.random() * 10) % 6 + 1;
+let secondDiceNumber = Math.floor(Math.random() * 10) % 6 + 1;
+console.log("3. Addition of 2 random dice numbers(1-6): ", firstDiceNumber+secondDiceNumber);
