358df7db8ac78891124f1e7ab08ad9d1a17e8afa
Harsh R Jain <rjainharsh@gmail.com>
2021-10-06 21:24:55 +0530

    ADDED: program to get word from single digit number

---
+++ SwitchCaseProblems/SingleDigitToWord.js
@@ -0,0 +1,13 @@
+let singleDigitNumber = Math.floor(Math.random() * 10) % 10;
+switch(singleDigitNumber) {
+    case 0: console.log("1. zero"); break;
+        case 1: console.log("1. one"); break;
+        case 2: console.log("1. two"); break;
+        case 3: console.log("1. three"); break;
+        case 4: console.log("1. four"); break;
+        case 5: console.log("1. five"); break;
+        case 6: console.log("1. six"); break;
+        case 7: console.log("1. seven"); break;
+        case 8: console.log("1. eight"); break;
+        case 9: console.log("1. nine"); break;
+}
