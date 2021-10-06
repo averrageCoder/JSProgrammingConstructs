dbf4a059d771db0eef8f41dfe43c0bd3011ad588
Harsh R Jain <rjainharsh@gmail.com>
2021-10-06 20:53:35 +0530

    ADDED: program to find sum and average of 5 random numbers

---
+++ SequenceProblems/SumAndAverageOf5Numbers.js
@@ -0,0 +1,14 @@
+let n1 = Math.floor(Math.random() * (99-10+1)+10);
+let n2 = Math.floor(Math.random() * (99-10+1)+10);
+let n3 = Math.floor(Math.random() * (99-10+1)+10);
+let n4 = Math.floor(Math.random() * (99-10+1)+10);
+let n5 = Math.floor(Math.random() * (99-10+1)+10);
+let sum = n1+n2+n3+n4+n5;
+let avg = sum/5;
+console.log("n1 : ", n1);
+console.log("n2 : ", n2);
+console.log("n3 : ", n3);
+console.log("n4 : ", n4);
+console.log("n5 : ", n5);
+
+console.log("4. Sum: ",sum," Average: ", avg);
