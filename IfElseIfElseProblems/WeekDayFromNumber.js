210f7934974266c5a6bfd42e8a257885d9626d00
Harsh R Jain <rjainharsh@gmail.com>
2021-10-06 21:20:39 +0530

    ADDED: program to get week day name from week day number

---
+++ IfElseIfElseProblems/WeekDayFromNumber.js
@@ -0,0 +1,15 @@
+let weekNumber = Math.floor(Math.random() * 7) % 7 + 1;
+if(weekNumber==1)
+    console.log("2. Monday")
+else if(weekNumber==2)
+    console.log("2. Tuesday")
+else if(weekNumber==3)
+    console.log("2. Wednesday")
+else if(weekNumber==4)
+    console.log("2. Thursday")
+else if(weekNumber==5)
+    console.log("2. Friday")
+else if(weekNumber==6)
+    console.log("2. Saturday")
+else
+    console.log("2. Sunday")
